0f7ad402f81dd042c3c4b5fbf43721aa6717d540
Snake <18520996@gm.uit.edu.vn>
2020-01-04 22:03:36 +0700

    ok

---
+++ src/astrobleme/logic/GameLoop.java
@@ -0,0 +1,43 @@
+package astrobleme.logic;
+
+import astrobleme.gui.Painter;
+import javafx.scene.canvas.GraphicsContext;
+
+public class GameLoop implements Runnable{
+    private final Grid grid;
+    private final GraphicsContext context;
+    private int frameRate;
+    private float interval;
+    private boolean running;
+    private boolean paused;
+    private boolean keyIsPressed;
+    public GameLoop(final Grid grid, final GraphicsContext context){
+        this.grid=grid;
+        this.context=context;
+        frameRate = 20;
+        interval = 1000 / frameRate;
+        running = true;
+        paused = false;
+        keyIsPressed = false;
+    }
+    @Override
+    public void run(){
+        while (running && paused){
+            // Time the update and paint calls
+            float time = System.currentTimeMillis();
+            
+            keyIsPressed = false;
+            grid.update();
+            Painter.paint(grid,context);
+            
+            if(!grid.getSnake()){
+                pause();
+                Painter.paintResetMessage(context);
+            }
+        }
+    }
+
+    private void pause() {
+         paused = true;
+    }
+}
